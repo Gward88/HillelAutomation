8bdda1bfab9e50f09259db8f824c244717ce854e
Andrey Lisitsa <andrey.lisitsa@rubyplay.com>
2020-06-09 10:13:40 +0300

    added lesson4 Homework

---
+++ src/test/java/lesson4/Homework.java
@@ -0,0 +1,28 @@
+package lesson4;
+
+public class Homework {
+    public static void main(String[] args) {
+        testListMethods(1);
+        testListMethods(Homework.class);
+    }
+    public static void testListMethods(int b){
+
+    }
+
+    public static void testListMethods(double b){
+
+    }
+
+    public static void testListMethods(String b){
+
+    }
+    public static void testListMethods(int b, int a){
+
+    }
+    public static void testListMethods(Integer b){
+
+    }
+    public static void testListMethods(Object b){
+
+    }
+}
